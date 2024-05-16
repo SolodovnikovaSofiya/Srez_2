00ad8460d6f2149ffd6817ff8a24a9f7fe7832c2
София Солодовникова <solodovnikovasofiya98@gmail.com>
2024-05-16 11:05:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+
+
+class Animal {
+protected:
+    std::string name;
+
+public:
+    Animal(const std::string& name) : name(name) {}
+
+    virtual void Voice() const {
+        std::cout << "Животное издает звук" << std::endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    Dog(const std::string& name) : Animal(name) {}
+
+    void Voice() const override {
+        std::cout << name << " лает: Гав-гав!" << std::endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    Cat(const std::string& name) : Animal(name) {}
+
+    void Voice() const override {
+        std::cout << name << " мяукает: Мяу-мяу!" << std::endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Animal* animal1 = new Dog("Шарик");
+    Animal* animal2 = new Cat("Мурка");
+
+    animal1->Voice();
+    animal2->Voice();
+
+    delete animal1;
+    delete animal2;
+
+    return 0;
+}
